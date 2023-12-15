80c567f33ce3171700b6cfc1026da1ea7ef61714
ditclay <ditclayl@gmail.com>
2023-12-16 01:01:01 +0300

    stul

---
+++ KompStul/Classes/Connect.cs
@@ -0,0 +1,14 @@
+﻿using KompStul.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KompStul.Classes
+{
+    class Connect
+    {
+        public static KompStulEntities modeldb;
+    }
+}
---
+++ KompStul/OrdersWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using KompStul.Classes;
+using KompStul.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KompStul
+{
+    /// <summary>
+    /// Логика взаимодействия для OrdersWindow.xaml
+    /// </summary>
+    public partial class OrdersWindow : Window
+    {
+        public OrdersWindow()
+        {
+            InitializeComponent();
+            MainFrame1.Navigate(new OrdersPages());
+            Manager.MainFrame1 = MainFrame1;
+
+        }
+
+        private void OpenWindow(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
